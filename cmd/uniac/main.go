248a37f709d6a00cca763a73d990e19d9cfd376d
AP2015 <arian.press2015@gmail.com>
2025-08-07 02:13:38 +0330

    refactor(cli): move cli handling to a separate module

--- cmd/uniac/main.go
+++ cmd/uniac/main.go
@@ -4,55 +4,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/arian-press2015/uniac/internal/loader"
-	"github.com/arian-press2015/uniac/pkg/mappings"
-	"github.com/spf13/cobra"
+	"github.com/arian-press2015/uniac/internal/cli"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "uniac",
-	Short: "uniac - Unified Infrastructure as Code",
-	Long:  "uniac is a tool for managing infrastructure as code using a unified language",
-}
-
-var validateCmd = &cobra.Command{
-	Use:   "validate [config-file]",
-	Short: "Validate a configuration file",
-	Long:  "Validate reads a configuration file(default: infra.yaml) and shows errors if any found",
-	Run: func(cmd *cobra.Command, args []string) {
-		var filepath string
-		if len(args) > 0 {
-			filepath = args[0]
-		} else {
-			filepath = "infra.yaml"
-		}
-
-		loader, err := loader.NewLoader(filepath)
-		if err != nil {
-			fmt.Println("Error creating config loader:", err)
-			os.Exit(1)
-		}
-
-		w, err := loader.Load(filepath)
-		if err != nil {
-			fmt.Println("Error loading and validating:", err)
-			os.Exit(1)
-		}
-
-		fmt.Println("World parsed and validated successfully")
-
-		config, err := mappings.GenerateIaCConfig(w, "example-cloud-provider", "terraform")
-
-		fmt.Println("results:", config, err)
-	},
-}
-
-func init() {
-	rootCmd.AddCommand(validateCmd)
-}
-
 func main() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := cli.RunCLI(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
---
+++ internal/cli/cli.go
@@ -0,0 +1,54 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/arian-press2015/uniac/internal/loader"
+	"github.com/arian-press2015/uniac/pkg/mappings"
+	"github.com/spf13/cobra"
+)
+
+func RunCLI() error {
+	var rootCmd = &cobra.Command{
+		Use:   "uniac",
+		Short: "uniac - Unified Infrastructure as Code",
+		Long:  "uniac is a tool for managing infrastructure as code using a unified language",
+	}
+
+	var validateCmd = &cobra.Command{
+		Use:   "validate [config-file]",
+		Short: "Validate a configuration file",
+		Long:  "Validate reads a configuration file(default: infra.yaml) and shows errors if any found",
+		Run: func(cmd *cobra.Command, args []string) {
+			var filepath string
+			if len(args) > 0 {
+				filepath = args[0]
+			} else {
+				filepath = "infra.yaml"
+			}
+
+			loader, err := loader.NewLoader(filepath)
+			if err != nil {
+				fmt.Println("Error creating config loader:", err)
+				os.Exit(1)
+			}
+
+			w, err := loader.Load(filepath)
+			if err != nil {
+				fmt.Println("Error loading and validating:", err)
+				os.Exit(1)
+			}
+
+			fmt.Println("World parsed and validated successfully")
+
+			config, err := mappings.GenerateIaCConfig(w, "example-cloud-provider", "terraform")
+
+			fmt.Println("results:", config, err)
+		},
+	}
+
+	rootCmd.AddCommand(validateCmd)
+
+	return rootCmd.Execute()
+}
